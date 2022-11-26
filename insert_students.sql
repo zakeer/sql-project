1e43fcf4353ac48270602579315082d4b77fc485
Syed Zakeer Hussain <syed.zakeer.hussain@sap.com>
2022-11-26 10:14:30 +0530

    sql project setup

---
+++ insert_students.sql
@@ -0,0 +1,6 @@
+INSERT INTO student VALUES 
+(1, 'syed', '10th', 43, 'male'),
+(2, 'hussain', '8th', 44, 'male'),
+(3, 'divya', '9th', 84, 'female'),
+(4, 'koti', '8th', 99, 'male'),
+(5, 'apsar', '6th', 44, 'male');
---
+++ student.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "student" (
+	"id"	INTEGER ,
+	"name"	TEXT NOT NULL,
+	"class"	TEXT NOT NULL,
+	"mark"	INTEGER NOT NULL DEFAULT 0,
+	"gender"	TEXT NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
